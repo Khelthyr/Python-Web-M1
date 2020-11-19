d0e2b1bd9a92895c08eb0643b3bc085565c7464c
Khelthyr <cedric.nils.vincendeau@gmail.com>
2020-11-19 13:38:41 +0100

    Added search id / titre with json file

--- TP2/app.py
+++ TP2/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Pour trouver le fichier JSON
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 json_url = os.path.join(SITE_ROOT, "book.json")
-data = json.load(open(json_url))
+jsonData = json.load(open(json_url))
 
 
 @app.route("/")
@@ -24,14 +24,25 @@ def route():
 def books():
     # result = request.json['book']
     # return jsonify({"book": result})
-    return data
+    return jsonData
 
 
 @app.route("/books id", methods=['POST', 'GET'])
 def booksid():
     if request.method == 'POST':
         idbook = request.form
-        return render_template('book.html', data=data, idbook=idbook)
+        return render_template('bookId.html', data=jsonData, idbook=idbook)
+    else:
+        return ("Il n'y a pas de livre avec cette id")
+
+        
+@app.route("/books titre", methods=['POST', 'GET'])
+def bookstitre():
+    if request.method == 'POST':
+        titre = request.form
+        return render_template('bookTitre.html', data=jsonData, titre=titre)
+    else:
+        return ("Il n'y a pas de livre avec ce titre")
 
 
 if __name__ == '__main__':
