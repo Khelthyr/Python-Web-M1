107a30ea8ecee4c9fdf61dbad85680a407a79691
Khelthyr <cedric.nils.vincendeau@gmail.com>
2020-11-19 10:08:56 +0100

    Add template / route (quickstart du TP)

--- TP2/app.py
+++ TP2/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "hello world!"
+    return render_template('index.html')
+
+
+@app.route("/ma route", methods=['POST', 'GET'])
+def route():
+    if request.method == 'POST':
+        name = request.form
+        return render_template('route.html', result=name)
 
 
 if __name__ == '__main__':
